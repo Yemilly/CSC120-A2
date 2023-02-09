16dd8cf1607a1a50ba98938c55e44f788127a4a7
Yemilly Soto <ysoto@smith.edu>
2023-02-08 21:59:47 -0500

    #1

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,6 +1,9 @@
+from computer import *
+
 class ResaleShop:
 
     # What attributes will it need?
+    inventory=[]
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
@@ -8,3 +11,10 @@ class ResaleShop:
         pass # You'll remove this when you fill out your constructor
 
     # What methods will you need?
+    def buy(self): Computer
+        
+    # 1. call computer(...) constructor 
+    #    to create a new computer instance
+
+    # 2. call inventory.append(...) to add the 
+    # new computer insatnce to the inventory. 
